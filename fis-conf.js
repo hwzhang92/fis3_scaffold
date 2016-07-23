c6677bf1f50c86dbbfe3ec3f149247777771dfab
张洪伟 <zhanghw@firstshare.cn>
2016-07-23 17:19:03 +0800

    增加动态mock功能

--- fis-conf.js
+++ fis-conf.js
@@ -6,6 +6,10 @@ fis.match('/static/sea.js', {
   isMod: false
 });
 
+fis.match('/mock/*.js',{
+	isMod: false
+});
+
 fis.hook('cmd', {
   paths: {
     "jquery": "/component_modules/jquery/1.10.1/jquery.js",
